e9aafd9c2447ca709756a3928128cad1b1e23995
Segni Mideksa <segnim21@gmail.com>
2026-02-09 00:58:39 +0300

    add new file about props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/Userprofile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,6 +18,11 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+        <UserProfile
+        name="Alice"
+        age="25" 
+        bio="Loves hiking and photography"
+        />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
