7145c57a4fc1ff38e800c135f79ccfe57d4d6387
Segni Mideksa <segnim21@gmail.com>
2026-02-09 00:43:25 +0300

    add and delete files

--- alx-react-app/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-function Header() {
-    return (
-       <div>
-        <header>My Favorite Cities</header>
-       </div>
-    );
-}
-export default Header;
--- alx-react-app/src/MainContent.jsx
+++
@@ -1,8 +0,0 @@
-function MainContent() {
-    return (
-        <main><div><p>I love to visit New York, Paris, and Tokyo.</p>
-        </div></main> 
-    );
-}
-
-export default MainContent;
