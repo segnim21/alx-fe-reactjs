9e48c19d5864113ed685d3e890546da45e1f8079
Segni Mideksa <segnim21@gmail.com>
2026-02-16 23:33:13 +0300

    s

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#eee' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#eee',  display: "justifyContent" }}>
       <Link to="/">Home</Link> | 
       <Link to="/about">About</Link> | 
       <Link to="/services">Services</Link> | 
