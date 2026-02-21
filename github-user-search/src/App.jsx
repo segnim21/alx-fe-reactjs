d4f65fce3e70be1bd151d714bf7157e9e7d3c404
Segni Mideksa <segnim21@gmail.com>
2026-02-22 00:58:20 +0300

    Add new branch about API

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return(
+    <div>
+      <h1>Github User Search</h1>
+      <p>Search for a Github user by username</p>
+      {/*comment for jsx*/}
+    </div>
+  )
+};
+
+export default App;
