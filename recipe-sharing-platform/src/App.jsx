d38a265d2b51ec656a9b0b326873f18917075eed
Segni Mideksa <segnim21@gmail.com>
2026-02-25 11:28:46 +0300

    Building homepage of recipe sharing platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,15 @@
+import HomePage from "./components/HomePage"
+
 function App() {
   return (
+    
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
         <h1 className="text-3xl font-bold text-center mb-8 text-blue-600">
           Recipe Sharing Platform 🍳
         </h1>
-        
+        {/*the homepage component will be renderd here*/}
+        <HomePage />
         <div className="space-y-4">
           <div className="border-b pb-4">
             <h2 className="text-xl font-semibold mb-2">Welcome!</h2>
